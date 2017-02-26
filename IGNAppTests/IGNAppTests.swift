550f7557617019375b24a055609d4e9346df4237
Bradley Scott French <bradleyfrench@bradleys-macbook-pro.local>
2017-02-26 18:28:26 -0500

    Initial Commit

---
+++ IGNAppTests/IGNAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  IGNAppTests.swift
+//  IGNAppTests
+//
+//  Created by Bradley French on 2/26/17.
+//  Copyright © 2017 Bradley French. All rights reserved.
+//
+
+import XCTest
+@testable import IGNApp
+
+class IGNAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
